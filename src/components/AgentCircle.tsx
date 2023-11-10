d3f2ba4181481ce3f9925e91d6725fdd19757f0f
Otto Heldt <otto.heldt@innofactor.com>
2023-11-10 23:13:14 +0200

    Init

---
+++ src/components/AgentCircle.tsx
@@ -0,0 +1,12 @@
+
+export const AgentCircle = () => {
+    return(
+        <div className={`container`}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <div className="after"></div>
+        </div>
+    )
+}
