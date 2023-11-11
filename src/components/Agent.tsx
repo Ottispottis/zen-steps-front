3a97668eb5348d84baf153156b28764fb996a74d
Ottispottis <otto.s.heldt@gmail.com>
2023-11-11 15:30:39 +0200

    Modified system prompt, textarea now clears on send

--- src/components/Agent.tsx
+++ src/components/Agent.tsx
@@ -6,12 +6,13 @@ import { Message } from "../types/Message";
 const systemPrompt: Message = {
 
     role: "system",
-    content: "You are a therapist giving a therapy session. Give me a 435 word inspirational meditation session about overcoming opioid addiction and chronic pain. Only output the actual session without ambiance instructions, quotation marks or 'Certainly, here is a 435-word inspirational meditation session on overcoming opioid addiction:'. Remember the key points for future reference."
+    content: "You are a therapist giving a therapy session. Give me a 130 word inspirational meditation session about overcoming chronic pain. Only output the actual session without ambiance instructions, quotation marks or 'Certainly, here is a 130-word inspirational meditation session on overcoming chronic pain:'. Remember the key points for future reference."
 }
 
 export const Agent = () => {
     const [conversation, setConversation] = useState<Message[]>([systemPrompt]);
     const [loadingResponse, setLoadingResponse] = useState<boolean>(false);
+    const [sendPressed, setSendPressed] = useState<boolean>(false);
 
     useEffect(() => {
         const handleSend = async () => {
@@ -24,45 +25,77 @@ export const Agent = () => {
                     //mode: 'no-cors',
                     headers: {
                         'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
                     },
                     body: JSON.stringify({ messages: conversation }),
                 });
     
                 if (!response.ok) {
                     setLoadingResponse(false);
+                    setSendPressed(false);
                     throw new Error(`HTTP error! Status: ${response.status}`);
                     
                 }
     
                 const data = await response.json();
                 setLoadingResponse(false);
+                setSendPressed(false);
                 console.log('API response:', data);
                 setConversation((prevMessages) => [...prevMessages, { role: 'assistant', content: data.content}]);
+                //makeSpeech(data.content)
             } catch (error: any) {
+                setSendPressed(false);
                 setLoadingResponse(false);
                 console.error('Error sending API request:', error.message);
             }
         };
-    
-        if (conversation.length > 1) {
-            handleSend();
-            // Clear messages after sending
-        }
-    }, [conversation]);
-    
-    
 
+        
+    
+        if (sendPressed) {
+            handleSend();
+        }
+    }, [sendPressed]);
+
+    const makeSpeech = async (content: string) => {
+        const audio = new Audio();
+        try {
+            const response = await fetch('http://192.168.159.231:3000/api/v1/speech', {
+                method: 'POST',
+                //mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify({ content }),
+            });
+
+            if (!response.ok) {
+                setLoadingResponse(false);
+                setSendPressed(false);
+                throw new Error(`HTTP error! Status: ${response.status}`);
+                
+            }
+            
+            const data = await response.blob();
+            const url = window.URL.createObjectURL(data)
+            audio.src = url
+            audio.play();
+            console.log(data);
+            
+        } catch (error: any) {
+            setSendPressed(false);
+            setLoadingResponse(false);
+            console.error('Error sending API request:', error.message);
+        }
+
+    }
+    
     const handleMessage = (message: Message) => {
         
       setConversation((prevMessages) => [...prevMessages, message]);
+      setSendPressed(true);
     };
-
-    const backgroundStyle = {
-        backgroundImage: "url('public/layered-waves-hakei.svg')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-      };
     
     return(
         <div className="flex flex-col w-screen bg-slate-900 pt-4 h-full items-center justify-evenly relative ">
@@ -73,7 +106,25 @@ export const Agent = () => {
                 <AgentCircle/>   
             </div>
             {loadingResponse && 
-            <div>Loading</div>}
+            <div className="flex relative">
+                <div className="loader">
+                    <div className="cell d-0"></div>
+                    <div className="cell d-1"></div>
+                    <div className="cell d-2"></div>
+
+                    <div className="cell d-1"></div>
+                    <div className="cell d-2"></div>
+
+
+                    <div className="cell d-2"></div>
+                    <div className="cell d-3"></div>
+
+
+                    <div className="cell d-3"></div>
+                    <div className="cell d-4"></div>
+                </div>
+            </div>
+            }
             <div className="flex w-6/12">
                 <ChatMessage message={handleMessage}/>
             </div>
--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Message } from "../types/Message";
 
 interface ChatMessageProps {
@@ -6,10 +6,10 @@ interface ChatMessageProps {
 
 }
 
-
 export const ChatMessage = (props: ChatMessageProps) => {
     const [message,setMessage] = useState<Message>()
     const [disabledButton,setDisabledButton] = useState<boolean>()
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
     const textAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setMessage({role:'user', content:event.currentTarget.value});
@@ -26,12 +26,15 @@ export const ChatMessage = (props: ChatMessageProps) => {
     const handleSend = () => {
         if(message){
             props.message(message);
+            if(textAreaRef.current){
+                textAreaRef.current.value = '';
+            }
         }
     }
 
     return(
         <div className="flex flex-row flex-1 gap-1 rounded bg-slate-800 p-1 items-center">
-            <textarea onChange={textAreaChange} placeholder="What's on your mind?" className="text-slate-400 rounded bg-transparent border border-slate-700 outline-none flex-grow resize-none overflow-hidden"></textarea>
+            <textarea ref={textAreaRef} onChange={textAreaChange} placeholder="What's on your mind?" className="text-slate-400 rounded bg-transparent border border-slate-700 outline-none flex-grow resize-none overflow-hidden"></textarea>
             <button onClick={handleSend} disabled={disabledButton} className={`rounded hover:bg-slate-700 hover:bg-opacity-80 px-2 h-6 text-xs`}>
                 Send
             </button>
