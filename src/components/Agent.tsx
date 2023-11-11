b834722e5adcd39094900eddd9ae24a6263ff31b
Ottispottis <otto.s.heldt@gmail.com>
2023-11-11 12:31:01 +0200

    Modifies ui, API post added

--- src/components/Agent.tsx
+++ src/components/Agent.tsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
 import { AgentCircle } from "./AgentCircle"
 import { ChatMessage } from "./ChatMessage";
 
 export const Agent = () => {
+    const [messages, setMessages] = useState<string[]>([]);
+    useEffect(() =>{
+        const handleSend = async () => {
+            console.log("API CALL")
+            try {
+              const response = await fetch('https://zen-steps.onrender.com/api/v1/openai', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ content: messages }),
+              });
+        
+              if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+              }
+        
+              const data = await response.json();
+              console.log('API response:', data);
+            } catch (error: any) {
+              console.error('Error sending API request:', error.message);
+            }
+          };
+          if(messages.length>0){
+            handleSend();  
+          }
+    },[messages])
+    
+
+        const handleMessage = (message: string) => {
+            
+          setMessages((prevMessages) => [...prevMessages, message]);
+        };
 
     const backgroundStyle = {
         backgroundImage: "url('public/layered-waves-hakei.svg')",
@@ -9,21 +43,16 @@ export const Agent = () => {
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
       };
-
-    const handleMessage = (message: string) => {
-        console.log(message);
-
-    }
     
     return(
         <div className="flex flex-col w-screen bg-slate-900 pt-4 h-full items-center justify-evenly relative ">
             <div className="flex">
-                <h1 className=" text-slate-300 text-4xl">Zen Steps</h1>
+                <h1 className=" text-slate-500 text-4xl">Zen Steps</h1>
             </div>
                 <div className="flex relative">
                     <AgentCircle/>   
                 </div>
-            <div className="flex">
+            <div className="flex w-6/12">
                 <ChatMessage message={handleMessage}/>
             </div>
             
--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 interface ChatMessageProps {
     message: (message: string) => void;
 
@@ -5,10 +7,33 @@ interface ChatMessageProps {
 
 
 export const ChatMessage = (props: ChatMessageProps) => {
+    const [message,setMessage] = useState<string>()
+    const [disabledButton,setDisabledButton] = useState<boolean>()
+
+    const textAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(event.currentTarget.value);
+    }
+
+    useEffect(()=>{
+        if(!message || message.length<2){
+            setDisabledButton(true);
+        }else{
+            setDisabledButton(false);
+        }
+    },[message])
+
+    const handleSend = () => {
+        if(message){
+            props.message(message);
+        }
+    }
+
     return(
-        <div className="flex flex-row gap-2">
-            <input type="text" className="rounded bg-slate-500 focus:outline-slate-700"></input>
-            <button >Submit</button>
+        <div className="flex flex-row flex-1 gap-1 rounded bg-slate-800 p-1 items-center">
+            <textarea onChange={textAreaChange} placeholder="What's on your mind?" className="text-slate-400 rounded bg-transparent border border-slate-700 outline-none flex-grow resize-none overflow-hidden"></textarea>
+            <button onClick={handleSend} disabled={disabledButton} className={`rounded hover:bg-slate-700 hover:bg-opacity-80 px-2 h-6 text-xs`}>
+                Send
+            </button>
         </div>
     )
 
